ffcd7835a3d02125af13f0e0b47125be54830c66
David Salomon <david35mm@pm.me>
2021-02-18 08:44:59 -0500

    Pushed my dotfiles before reinstall

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -18,31 +18,27 @@ set PATH $PATH /usr/local/go/bin:$HOME/.emacs.d/bin:$HOME/.local/bin:$HOME/Xiaom
 
 # Functions needed for !! and !$
 function __history_previous_command
-  switch (commandline -t)
-  case "!"
-    commandline -t $history[1]; commandline -f repaint
-  case "*"
-    commandline -i !
-  end
+	switch (commandline -t)
+	case "!"
+    	commandline -t $history[1]; commandline -f repaint
+	case "*"
+    	commandline -i !
+	end
 end
 
 function __history_previous_command_arguments
-  switch (commandline -t)
-  case "!"
-    commandline -t ""
-    commandline -f history-token-search-backward
-  case "*"
-    commandline -i '$'
-  end
+	switch (commandline -t)
+	case "!"
+    	commandline -t ""
+    	commandline -f history-token-search-backward
+	case "*"
+    	commandline -i '$'
+	end
 end
+
 # The bindings for !! and !$
-if [ $fish_key_bindings = fish_vi_key_bindings ];
-  bind -Minsert ! __history_previous_command
-  bind -Minsert '$' __history_previous_command_arguments
-else
-  bind ! __history_previous_command
-  bind '$' __history_previous_command_arguments
-end
+bind ! __history_previous_command
+bind '$' __history_previous_command_arguments
 
 # Function for extracting archives
 function ex
