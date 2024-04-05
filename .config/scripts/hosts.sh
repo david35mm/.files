82a5a65ef80d0e33c29050bb3838858be9e66d55
David Andrés Ramírez Salomón <66393355+david35mm@users.noreply.github.com>
2024-04-05 11:49:16 -0500

    script doesn't echo hosts file to stdout no more

--- .config/scripts/hosts.sh
+++ .config/scripts/hosts.sh
@@ -1,4 +1,9 @@
 #!/bin/sh
 
-sudo sed -i 7q /etc/hosts
-curl -s "https://divested.dev/hosts" | sudo tee -a /etc/hosts
+if [ "$(whoami)" = root ]; then
+  sed -i 7q /etc/hosts
+  curl -fsSL "https://divested.dev/hosts" >> /etc/hosts
+else
+  printf '%b\n' "\n\t\033[0;31m\033[1m●  Error! \033[0m You must run this script as the \033[0;31mroot\033[0m user\n"
+  exit 1
+fi
