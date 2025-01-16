e0e040be267ae9cb6ed98b6896aef130d80a3984
David Andrés <66393355+david35mm@users.noreply.github.com>
2025-01-15 21:52:50 -0500

    Update setup_iwd.sh

--- .config/scripts/setup_iwd.sh
+++ .config/scripts/setup_iwd.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-VERSION_CONTROL=numbered
+export VERSION_CONTROL=numbered
 
 sudo dnf --refresh -y in iwd
 sudo systemctl mask wpa_supplicant
@@ -30,22 +30,22 @@ sudo mv -vb /etc/systemd/resolved.conf /etc/systemd/resolved.conf.bak
 cat << EOF | sudo tee /etc/systemd/resolved.conf
 [Resolve]
 DNS=9.9.9.9#dns.quad9.net 149.112.112.112#dns.quad9.net 2620:fe::fe#dns.quad9.net 2620:fe::9#dns.quad9.net
-FallbackDNS=1.1.1.2#security.cloudflare-dns.com 1.0.0.2#security.cloudflare-dns.com 2606:4700:4700::1112#security.cloudflare-dns.com 2606:4700:4700::1002#security.cloudflare-dns.com
-DNSOverTLS=yes
+FallbackDNS=194.242.2.5#extended.dns.mullvad.net 2a07:e340::5#extended.dns.mullvad.net
 DNSSEC=allow-downgrade
+DNSOverTLS=opportunistic
 Domains=~.
 EOF
+
+sudo ln -sf /run/systemd/resolve/stub-resolv.conf /etc/resolv.conf
 
 sudo mv -vb /etc/systemd/timesyncd.conf /etc/systemd/timesyncd.conf.bak
 cat << EOF | sudo tee /etc/systemd/timesyncd.conf
 [Time]
 NTP=ntp1.inm.gov.co ntp2.inm.gov.co
-FallbackNTP=1.co.pool.ntp.org 0.south-america.pool.ntp.org 3.south-america.pool.ntp.org
+FallbackNTP=0.co.pool.ntp.org 0.south-america.pool.ntp.org 1.south-america.pool.ntp.org
 EOF
 
 sudo systemctl enable --now systemd-timesyncd
 sudo timedatectl set-ntp true
 sudo hwclock --systohc
 sudo timedatectl set-local-rtc 0
-
-# sudo ln -sf /run/systemd/resolve/stub-resolv.conf /etc/resolv.conf
